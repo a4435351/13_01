e70465c6cf4c29317e92838a089cf808697b16d1
dungtv <dungtv@elephas.vn>
2017-11-15 14:47:25 +0700

    v8

--- dynamic_listview_advance_odoo_v81/__openerp__.py
+++ dynamic_listview_advance_odoo_v81/__openerp__.py
@@ -1,10 +1,10 @@
 {
-    'name': 'First Status',
+    'name': 'Dynamic List Odoo 8',
     'summary': 'Change The Odoo List view On the fly without any technical knowledge',
     'version': '1.0',
     'category': 'Web',
     'description': """
-
+        Change The Odoo List view On the fly without any technical knowledge
     """,
     'author': "",
     'depends': ['web'],
@@ -18,6 +18,6 @@
     'application': False,
     'qweb': ['static/src/xml/listview_button_view.xml'],
     'images': [
- 
+        'static/description/main-image.png'
     ],
 }
